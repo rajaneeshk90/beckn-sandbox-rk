d546bfc3a9eca5a89b5a91fc34cd33cd395126f1
Rajaneesh <rajaneeshk90@gmail.com>
2024-05-13 19:58:14 +0530

    Creating the sandbox for student connect: dsep Adding postman collection for the student connect

--- src/dsep/dsep.service.ts
+++ src/dsep/dsep.service.ts
@@ -5,6 +5,7 @@ import { DomainNotFoundException } from "../common/exception/domain-notfound.exc
 import { JobsInternshipsService } from "./jobs-internships/jobs-internships.service";
 import { TutoringMentorshipService } from "./tutoring-mentorship/tutoring-mentorship.service";
 import { CoursesTrainingService } from "./courses-training/courses-training.service";
+import { StudentConnectService } from "./student-connect/student-connect.service";
 import { DSEPApiInterface } from "./dsep-api.interface";
 @Injectable()
 export class DsepService {
@@ -18,6 +19,8 @@ export class DsepService {
                 return new CoursesTrainingService();
             case DSEPModulesEnum.TUTORING_MENTORSHIPS:
                 return new TutoringMentorshipService();
+            case DSEPModulesEnum.STUDENT_CONNECT:
+                return new StudentConnectService();
             case DSEPModulesEnum.ONEST_WORK_OPPORTUNITIES:
                 return new JobsInternshipsService();
             case DSEPModulesEnum.ONEST_LEARNING_EXPERIENCES:
