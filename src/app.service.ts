34fa86184c467fb41dabe45bcc29a2191ba96905
Rajaneesh <rajaneeshk90@gmail.com>
2024-05-27 20:11:38 +0530

    adding city of light capability in sandbox

--- src/app.service.ts
+++ src/app.service.ts
@@ -28,6 +28,8 @@ export class AppService {
                 domainPath = `tourism`;
             } else if (body.context.domain.includes("localretail:shopping")) {
                 domainPath = `dsnp`;
+            } else if (body.context.domain.includes('retail:CityOfLight')) {
+                domainPath = `city-of-light`;
             } else if (body.context.domain.includes("retail")) {
                 const default_version = "1.1.0";
                 let version = default_version;
