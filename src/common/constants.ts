00687659ca5706ef046366756705a9ba2e8758b6
Shivani-Dhoke <shivani.dhoke@eminds.ai>
2024-05-29 14:01:01 +0530

    fixed some changes

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,2 +1,2 @@
-export const BPP_ID = "bpp-ps-network-sandbox-dev.becknprotocol.io";
-export const BPP_URI = "https://bpp-ps-network-sandbox-dev.becknprotocol.io";
+export const BPP_ID = "sandbox-bpp-network.becknprotocol.io";
+export const BPP_URI = "https://sandbox-bpp-network.becknprotocol.io";
